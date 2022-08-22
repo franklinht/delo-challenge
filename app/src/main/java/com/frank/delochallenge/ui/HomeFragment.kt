b07e4fadba051e7a57872c2692e7b771205c6935
Franklin Hirata <franklin.hirata@gmail.com>
2022-08-22 18:48:10 +0100

    add cached postalcodes

--- app/src/main/java/com/frank/delochallenge/ui/HomeFragment.kt
+++ app/src/main/java/com/frank/delochallenge/ui/HomeFragment.kt
@@ -7,14 +7,14 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
 import com.frank.delochallenge.R
-import com.frank.delochallenge.databinding.FragmentFirstBinding
+import com.frank.delochallenge.databinding.FragmentHomeBinding
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
-class FirstFragment : Fragment() {
+class HomeFragment : Fragment() {
 
-    private var _binding: FragmentFirstBinding? = null
+    private var _binding: FragmentHomeBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -25,7 +25,7 @@ class FirstFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
 
-        _binding = FragmentFirstBinding.inflate(inflater, container, false)
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
         return binding.root
 
     }
